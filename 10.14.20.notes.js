0e9253667d0d8bc73269a86e82442b357bbae959
mauricejthomas <mauricejthomas@gmail.com>
2020-10-17 06:55:44 -0400

    10.14.20-notes

---
+++ 10.14.20.notes.js
@@ -0,0 +1,9 @@
+let name = 'Maurice Thomas'
+let fullName = name.split(" ")
+console.log(fullName)
+let firstName = fullName[0]
+let lastName = fullName[1]
+console.log(firstName[0])
+console.log(lastName[0])
+console.log(firstName)
+console.log(lastName)
