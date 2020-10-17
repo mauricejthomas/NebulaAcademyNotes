d54b161359dcb027e3fa1c7ea1acf2b3c5ed364d
mauricejthomas <mauricejthomas@gmail.com>
2020-10-17 08:42:38 -0400

    10.17.20-hw

---
+++ 10.17.20.hw.js
@@ -0,0 +1,39 @@
+//1. "we are software engineers at nebula academy"
+let hwString = "we are software engineers at nebula academy"
+
+console.log(hwString.indexOf('software'))
+
+console.log(hwString.indexOf('s'))
+
+console.log(hwString.indexOf('web'))
+
+console.log(hwString.includes('z'))
+
+console.log(hwString.includes('engineer'))
+
+let hwSplit = hwString.split(' ')
+console.log(hwSplit)
+
+console.log(hwString.slice(7,15))
+
+console.log(hwString.slice(36,43))
+//1. "we are learning new string methods"
+let newHwString = "we are learning new string methods"
+
+console.log(newHwString.length)
+
+let lastChar = newHwString.length - 1
+console.log(newHwString.charAt(lastChar))
+
+console.log(newHwString[5])
+
+console.log(newHwString.replace(/a/g,"-"))
+
+console.log(newHwString.toUpperCase())
+
+console.log(newHwString.toLowerCase())
+//1.
+let chocolate = "My favorite ice cream flavor is chocolate"
+console.log(chocolate)
+
+
