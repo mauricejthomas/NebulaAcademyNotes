5de136ccb5fbfa8b740562108405d4ad75fb1eca
mauricejthomas <mauricejthomas@gmail.com>
2020-10-24 09:05:24 -0400

    10.24.20-hw

---
+++ Assignments/10.24.20.hw.js
@@ -0,0 +1,28 @@
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+console.log(days.length)
+
+console.log(days[3])
+
+console.log(days.shift())
+console.log(days)
+
+days.unshift('Sunday', )
+console.log(days)
+
+console.log(days.pop())
+console.log(days)
+console.log('saturday', )
+
+days.push('Saturday')
+console.log(days)
+
+days.splice(4,1, 'Friday Junior',)
+console.log(days)   
+
+let iDontKnow = '?'
+console.log(iDontKnow)
+
+let phone = ['iphone', 'android']
+let laptop = ['MacBook', 'HP', 'Dell']
+console.log(phone.concat(laptop))
