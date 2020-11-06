4945ceb07e553cb9df43b2ba3167c3b72ce76b60
mauricejthomas <mauricejthomas@gmail.com>
2020-11-05 23:25:51 -0500

    10.31.20-hw

--- Assignments/practice.js
+++ Assignments/practice.js
@@ -5,3 +5,21 @@ console.log(true ? 1 : 2)
 console.log("5" + 1)
 
 console.log(11%2)
+
+
+console.log(Math.sqrt(81))
+
+function lastTwoEle (TheNewArray){
+    return `${TheNewArray[TheNewArray.length - 1]}  ${TheNewArray[TheNewArray.length-2]}`
+}
+ console.log(lastTwoEle(['apple', 'banana', 'orange', 'peach', 'grape']))
+
+
+
+
+
+
+
+
+
+
