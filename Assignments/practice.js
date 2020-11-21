f0c504b597a53e392c827843d8f9df46b13385bb
mauricejthomas <mauricejthomas@gmail.com>
2020-11-21 11:41:06 -0500

    Function Notes

--- Assignments/practice.js
+++ Assignments/practice.js
@@ -1,58 +1,8 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var iifeExample = 'This is a string;';
 
-function practiceProblem (snipe){
-        for (let i = 0; i < arr.length; i++){
-            console.log(mat.a)
-        if (arr[i] > 20){
-            console.log(arr[i])
-        } else {
-            console.log('number does not satisfy')
-        }
-    }
-    return 'is it working'
-}
-console.log(onlyGreaterThanTwenty(numArray))
-
-// add all of the numbers in the array
-function addNums (array){
-
-}
-console.log(addNums(array))
-// add only the odd numbers in the array
-
-// count how many numbers are even in the array
-
-// add the numbers that are NOT divisible by 3
-
-
-// Part 1
-
-let numArray = [9, 12, 3, 0, 8, 50, 101, 8, 4, 15, 3, 2]
-
-// Problem # 1
-// Using the array above, create a function that loops through the array and return a new array with only the odd numbers
-
-function problemArray(arrayProblem){
-    for (let i = 0; i <= arrayProblem.length; i++){
-       if (i % 2 === 1)
-       console.log(arrayProblem[i], 'Odd')
-   }
-   return arrayProblem
-}
-console.log(problemArray(numArray))    
-
-// Problem # 2
-
-// Using the array above, loop through the array and COUNT the amount of even numbers that are in the array
-function problemArray(arrayProblem){
-    let placeholder = []
-    for (let i = 0; i <= arrayProblem.length; i++){
-       if (arrayProblem[i] % 2 === 0)
-       console.log([i])
-   }
-   return arrayProblem
- 
-console.log(problemArray(numArray))
+console.log(function(param){
+     return param.replace(/ /g,'-');
+ }(iifeExample)); // This-is-a-string;
 
 
 
