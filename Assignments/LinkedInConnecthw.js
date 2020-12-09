48c8e06a633440ccb4ea819b1444b5e7795c1101
mauricejthomas <mauricejthomas@gmail.com>
2020-12-09 17:04:05 -0500

    LinkedIn-hw

---
+++ Assignments/LinkedInConnecthw.js
@@ -0,0 +1,11 @@
+// Good Afternoon Karene,
+
+// My name is Maurice Thomas and I was one of the Nebula Academy students sitting in on your lecture on December 3rd.  
+// Thank you so much for taking the time to educate us on the interview process of obtaining employment. 
+// This is a new field for me, and it was interesting to learn the streamline of the interview as well as some of the specific things that companies look for when hiring.  
+// If possible, I would like to know if it would be ok to reach out to you if I have any additional questions in relation to obtaining employment in the field.  
+// Thank you once again for your time.
+
+// Best Regards,
+// Maurice J Thomas
+	
