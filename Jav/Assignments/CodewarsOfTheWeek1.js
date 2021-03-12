6c961ed85d305d2724c0552f299ae1087c0ba3cf
mauricejthomas <mauricejthomas@gmail.com>
2021-03-12 15:42:19 -0500

    Codewars OTW #1

---
+++ Jav/Assignments/CodewarsOfTheWeek1.js
@@ -0,0 +1,13 @@
+// Codewars Of The Week #1
+
+// Function (elevatorDistance) and parameter (array) are given
+function elevatorDistance(array) {
+// Create variable to serve as a placeholder
+    let placeHolder = 0;
+// Create for loop that loops through the length of the array, and returns absolute value of floors traveled and adds value to placeholder    
+    for (let i = 0; i < array.length - 1; i++) {
+      placeHolder += Math.abs(array[i] - array[i + 1]);
+    }
+// Return placeholder    
+    return placeHolder;
+}    
