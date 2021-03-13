541f23dd2262a49ea28f2b3e20746a024f163a3e
mauricejthomas <mauricejthomas@gmail.com>
2021-03-13 15:36:18 -0500

    Interview Questions

---
+++ Jav/Assignments/InterviewQuestionshw.js
@@ -0,0 +1,9 @@
+/* Interview Questions
+
+1. How would you describe the work/life balance at your organization?
+2. What are the best/worst things about working for your company?
+3. Where do you see this company in five years?
+4. What type of opportunites do you provide employees who want to move up in the company?
+5. Where do you see your company fitting in terms of the competitive landscape?
+
+*/
