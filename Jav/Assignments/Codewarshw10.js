7c386ed3c16eb1de595270ec83ecf1bf0a6115b1
mauricejthomas <mauricejthomas@gmail.com>
2021-03-13 13:31:06 -0500

    Codewars 10-hw

--- Jav/Assignments/Codewarshw10.js
+++ Jav/Assignments/Codewarshw10.js
@@ -1,55 +1,83 @@
-// Problem 1
+// Problem 1- Difference in Age
 
+// Function (differenceInAges) and parameter (ages) are given
 function differenceInAges(ages){
-
-    ages = ages.sort((a,b) => a -b);
-    
-    let youngest = ages[0];
-    let oldest = ages.pop();
-    
-    
-    return new Array(youngest, oldest, (oldest-youngest));
+// Use .sort method to put ages in ascending order
+    ages = ages.sort((a,b) => a - b);
+// Create variable to serve as lowest age    
+    let low = ages[0];
+// Create variable to remove last element of the array and return it    
+    let high = ages.pop();    
+// Return a new array with the youngest age, oldest age, and difference between the two
+    return new Array(low, high, (high - low));
     }
 
 
-// Problem 2
-function abbrevName(name){
+// Problem 2- Initials
 
-    const [firstName, lastName] = name.split(' ')
-    const firstInitial = firstName.charAt(0).toUpperCase()
-    const secondInitial = lastName.charAt(0).toUpperCase()
-    return firstInitial + '.' + secondInitial
+// Function (abbrevName) and (name) are given
+function abbrevName(name){
+// Crete variable that is an array that is split into substrings
+    let [first, last] = name.split(' ')
+// Create variable that finds the first character of 'first' and converts it to upper case    
+    let one = first.charAt(0).toUpperCase()
+// Create variable that finds the first character of 'last' and converts it to upper case    
+    let two = last.charAt(0).toUpperCase()
+// Return the strings together with period in between     
+    return one + '.' + two
 
 }
 
-// Problem 3
+
+// Problem 3- Needle in the hay stack
+
+// Function (findNeedle) and parameter (haystack) are given
 function findNeedle(haystack) {
+// Return string plus the index of "needle" in parameter 
     return "found the needle at position " + haystack.indexOf("needle");
   }
 
-// Problem 4
+
+// Problem 4- Count of positives sum of negatives
+
+// Function (countPositivesSumNegatives) and parameter (input) are given
 function countPositivesSumNegatives(input) {
+// Create condition that if length of parameter is strictly equal to null or 0, return an empty array  
     if (input === null || input.length === 0)
           return [];
-    let countOfPositives = 0;
-    let sumOfNegatives = 0;
+// Create variable to serve as placeholder for positive numbers          
+    let positivePlace = 0;
+// Create variable to serve as placeholder for negative numbers    
+    let negativePlace = 0;
+// Create for loop that loops through the entire array of integers    
     for(let i = 0; i < input.length; i++){
-    if(input[i] > 0){countOfPositives++;}
-      else {sumOfNegatives += input[i];}
+// Create condition that if the index value is greater than zero, add value to positive placeholder, otherwise add value to negative placeholder    
+    if(input[i] > 0){positivePlace++;}
+      else {negativePlace += input[i];}
     } 
-      let arr = [];
-      arr.push(countOfPositives);
-      arr.push(sumOfNegatives);
-    return arr;
-    }
+// Create variable that serves as placeholder for new array    
+      let placeHolder = [];
+// Push all positive values, then all negative values into new array
+      placeHolder.push(positivePlace);
+      placeHolder.push(negativePlace);
+// Return new array      
+  return placeHolder;
+}
 
-// Problem 5
+
+// Problem 5- Remove duplicate
+
+// Function (distinct) and parameter (a) are given
 function distinct(a) {
-    var uniquea= [];
-                 for(i=0; i < a.length; i++){
-     if(uniquea.indexOf(a[i]) === -1) {
-         uniquea.push(a[i]);
-     }
- }
- return uniquea;
+// Create variable to serve as placeholder for new array
+  let placeHolder= [];
+// Create for loop to loop through length of parameter  
+  for(i=0; i < a.length; i++){
+// Create condition that if value is not found in new array, push value into new array
+    if(placeHolder.indexOf(a[i]) === -1) {    
+      placeHolder.push(a[i]);
+    }
+  }
+// Return final array  
+  return placeHolder;
 }
--- Jav/Assignments/Codewarshw11.js
+++ Jav/Assignments/Codewarshw11.js
@@ -1,1 +1,55 @@
-f
+// Problem 1- What is between?
+function between(a, b) {
+    // your code here
+    arr = []
+    for(i = a;i <= b; i++){ 
+        arr.push(i)
+    }
+    return arr
+  }
+
+
+
+
+// Problem 2- Tip Calculator
+function calculateTip(amount, rating) {
+    switch(rating.toLowerCase()){
+      case "terrible":return 0;
+      case "poor":return Math.ceil(amount * 0.05);
+      case "good":return Math.ceil(amount * 0.1);
+      case "great":return Math.ceil(amount * 0.15);
+      case "excellent":return Math.ceil(amount * 0.2);
+      default:return "Rating not recognised";
+    }
+  }
+
+
+
+
+// Problem 3- Draw Stairs
+function drawStairs(n) {
+    let result = [];
+    
+    for (let i = 0; i < n; i++) {
+      result[i] = `${' '.repeat(i)}I`;
+    }
+    
+    return result.join('\n');
+  }
+
+
+
+// Problem 4- Remove String Spaces
+function noSpace(x){
+    var result = ""
+    for(var index = 0; index < x.length; index++){
+      if(x[index] !== " "){
+        result += x[index];
+      }
+    }
+    return result;
+  }
+
+
+
+// Problem 5- TwoSum
