7eb87fdbee0e7c140c7c2c957ce2147737df62b6
mauricejthomas <mauricejthomas@gmail.com>
2021-06-20 13:12:02 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac16.js
@@ -0,0 +1,11 @@
+// Multiplication table for number
+
+const multiTable = (number) => {
+    let table = '';
+    
+    for(let i = 1; i <= 10; i++) {
+      table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+    }
+  
+    return table;
+}
