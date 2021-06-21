f4195e30f864cfb2414e289460b40ed99648f15f
mauricejthomas <mauricejthomas@gmail.com>
2021-06-21 18:53:30 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac27.js
@@ -0,0 +1,12 @@
+// Area or Perimeter
+
+const areaOrPerimeter = function(l, w) {
+    let area = 0;
+    let perimeter = 0;
+    
+    if (l == w) {
+      return area = l * w;
+    } else {
+      return perimeter = (l + w) * 2;
+    }
+};
