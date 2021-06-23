1ef1d5417f8642ebc30d9f29245899762645f553
mauricejthomas <mauricejthomas@gmail.com>
2021-06-22 22:28:34 -0400

    Codewars practice

--- Jav/Assignments/CWprac30.js
+++ Jav/Assignments/CWprac30.js
@@ -4,4 +4,4 @@ function abbrevName(name){
 
     var nameArray = name.split(" ");
     return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
-  }
+}
---
+++ Jav/Assignments/CWprac31.js
@@ -0,0 +1,6 @@
+// Do you speak "English"?
+
+function spEng(sentence){
+    let sentenceLower = sentence.toLowerCase();
+    return sentenceLower.includes('english');
+}
