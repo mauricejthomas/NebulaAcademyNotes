e67d6104d7b3079d0ebc4d120dc7779573a240ea
mauricejthomas <mauricejthomas@gmail.com>
2021-06-23 23:46:36 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac38.js
@@ -0,0 +1,14 @@
+// Drink about
+
+function peopleWithAgeDrink(age){
+    if(age < 14)
+      drink = 'toddy';
+    else if(age < 18)
+      drink = 'coke'
+    else if(age < 21)
+      drink = 'beer';
+    else if(age => 21)
+      drink = 'whisky';
+  
+    return 'drink ' + drink;
+  }
