2f24a03e0dab5812d5a95810628fcdcdde29f7aa
mauricejthomas <mauricejthomas@gmail.com>
2021-06-25 23:54:17 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac53.js
@@ -0,0 +1,12 @@
+// Lario and Muigi Pipe Problem
+
+function pipeFix(numbers){
+    var first = numbers[0];
+    var last = numbers[numbers.length-1];
+    
+    var arr = [];
+    for(var i = first; i <= last; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
