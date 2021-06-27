87df93419ca91c0a5a967b9f886e7d171fde052c
mauricejthomas <mauricejthomas@gmail.com>
2021-06-26 22:45:29 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac57.js
@@ -0,0 +1,18 @@
+// Is there a vowel in there?
+
+function isVow(a){
+    return a.map(i => {
+    if (String.fromCharCode(i) === 'a')
+    return 'a';
+    else if (String.fromCharCode(i) === 'e')
+    return 'e';
+    else if (String.fromCharCode(i) === 'i')
+    return 'i';
+    else if (String.fromCharCode(i) === 'o')
+    return 'o';
+    else if (String.fromCharCode(i) === 'u')
+    return 'u';
+    else 
+    return i;
+    })
+    }
