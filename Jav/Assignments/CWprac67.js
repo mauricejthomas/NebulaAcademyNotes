d3dd5903b74878ee30ba11e8c22b26ca76b1c6a8
mauricejthomas <mauricejthomas@gmail.com>
2021-06-27 11:00:05 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac67.js
@@ -0,0 +1,14 @@
+// Fake Binary
+
+function fakeBin(str){
+    var newStr = "";
+    for(var i=0;i<str.length;i++){
+      if(Number(str[i])>=5){
+        newStr += "1"
+      }
+      else{
+        newStr += "0";
+      }
+    }
+    return newStr;
+  }
