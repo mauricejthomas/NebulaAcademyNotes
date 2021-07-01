5df83782ce18095af1bdb66ea57bbce091d22f71
mauricejthomas <mauricejthomas@gmail.com>
2021-06-30 23:15:36 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac72.js
@@ -0,0 +1,13 @@
+// I love you, a little , a lot, passionately ... not at all
+
+function howMuchILoveYou(nbPetals) {
+    let phrase = {    
+      0: "not at all",
+      1: "I love you",
+      2: "a little",
+      3: "a lot",
+      4: "passionately",
+      5: "madly"
+    }
+    return phrase[nbPetals%6]
+  }
