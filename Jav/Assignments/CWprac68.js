c6d7e823e10f7904b8e7f3b712b44ff9a6ecf626
mauricejthomas <mauricejthomas@gmail.com>
2021-06-30 23:09:12 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac68.js
@@ -0,0 +1,14 @@
+// Transportation on vacation
+
+function rentalCarCost(d) {
+    var amount,
+      dailyCharge = 40;
+    if(d >= 7) {
+      amount = (dailyCharge * d) - 50;
+    } else if (d >= 3) {
+      amount = (dailyCharge * d) - 20;
+    } else {
+      amount = dailyCharge * d;
+    }
+    return amount;
+  }
