75bf804256def3911f8788301ad8399a01acfb95
mauricejthomas <mauricejthomas@gmail.com>
2021-07-01 23:52:28 -0400

    Codewars practice

---
+++ Jav/Assignments/CWprac80.js
@@ -0,0 +1,19 @@
+// Find Maximum and Minimum Values of a List
+
+var min = function(list){
+    var min =list[0];
+    for(var i=0; i<list.length; i++){
+       var cur = list[i];
+       if (cur<min) min=cur;
+    }
+    return min;
+}
+
+var max = function(list){
+    var max =list[0];
+    for(var i=0; i<list.length; i++){
+       var cur = list[i];
+       if (cur>max) max=cur;
+    }
+    return max;
+}
