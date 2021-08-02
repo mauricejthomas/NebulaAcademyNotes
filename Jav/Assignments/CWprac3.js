c5f5adb23abad7899a6635eb7969929a7adb39b1
mauricejthomas <mauricejthomas@gmail.com>
2021-08-01 23:33:50 -0400

    cwprac

--- Jav/Assignments/CWprac3.js
+++
@@ -1,5 +0,0 @@
-// Reversed Words
-
-function reverseWords(str){
-    return str.split(' ').reverse().join(' ');
-  }
--- Jav/Assignments/CWprac5.js
+++
@@ -1,7 +0,0 @@
-// Cat years, Dog years
-
-var humanYearsCatYearsDogYears = function(y) {
-    if (y == 1) return [1, 15, 15]
-    if (y == 2) return [2, 24, 24]
-    return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
-  }
